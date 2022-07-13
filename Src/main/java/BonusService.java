9ab6fa125b0d8aa41d6c61fefcfea9ac81a4f908
Liza <skomorohova-elizaveta@rambler.ru>
2022-07-13 19:50:33 +0300

    BonusTest

--- Src/main/java/BonusService.java
+++ Src/main/java/BonusService.java
@@ -6,17 +6,18 @@ public class BonusService {
         if (bonus > limit) { //1
             bonus = limit;
         }
-        if (bonus < limit) //2
-            if (amount == 0) { //3
-                System.out.println("Совершите покупку, чтобы получить бонусы!");
-            }
-        if (!registered) { //4
-        }
-        boolean b = amount < 99;
-        if (!registered && b) { //5
-            System.out.println("Суммы покупки недостаточно для начисления бонусов"); //4
-        }
+        //  if (bonus < limit) //2
+        //      if (amount == 0) { //3
+        //           System.out.println("Совершите покупку, чтобы получить бонусы!");
+        //        }
+        //   if (!registered) { //4
+        //   }
+        //   boolean b = amount < 99;
+        //  if (!registered && b) { //5
+        //      System.out.println("Суммы покупки недостаточно для начисления бонусов"); //4
+        //  }
 
         return bonus;
     }
 }
+//После вебинара с разбором поняла, что делала вообще не там) не в test)
