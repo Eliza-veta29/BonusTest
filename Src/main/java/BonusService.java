dfd2e9bfea1c1c3f0984781ba0269b1796d4477e
Liza <skomorohova-elizaveta@rambler.ru>
2022-07-13 18:22:36 +0300

    BonusTest

---
+++ Src/main/java/BonusService.java
@@ -0,0 +1,22 @@
+public class BonusService {
+    public long calculate(long amount, boolean registered) {
+        int percent = registered ? 3 : 1;
+        long bonus = amount * percent / 100;
+        long limit = 500;
+        if (bonus > limit) { //1
+            bonus = limit;
+        }
+        if (bonus < limit) //2
+            if (amount == 0) { //3
+                System.out.println("Совершите покупку, чтобы получить бонусы!");
+            }
+        if (!registered) { //4
+        }
+        boolean b = amount < 99;
+        if (!registered && b) { //5
+            System.out.println("Суммы покупки недостаточно для начисления бонусов"); //4
+        }
+
+        return bonus;
+    }
+}
---
+++ Src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+    BonusService service = new BonusService();
+        boolean registered = true;
+        long expected;
+        expected = 30;
+        long amount;
+        amount = 1000;
+        long actual = service.calculate(amount, registered);
+    }
+}
